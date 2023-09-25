0c065352bab9ff4d70b2fd54a7df47b566f7fd2e
Allen Russell <garyrusselljr@gmail.com>
2023-09-25 10:09:08 -0600

    test: happy path for book search spec - modify 'summary'

--- app/poros/book_search.rb
+++ app/poros/book_search.rb
@@ -1,10 +1,12 @@
 class BookSearch
-  attr_reader :destination, :forecast, :total_books_found, :books
+  attr_reader :id, :destination, :forecast, :total_books_found, :books
 
   def initialize(location, data)
+    @id = nil
     @destination = location
     @forecast = get_forecast
     @total_books_found = data[:numFound]
+    require 'pry';binding.pry
     @books = list_books(data)
   end
 
@@ -13,8 +15,8 @@ class BookSearch
     results = ForecastFacade.new(coordinates.lat, coordinates.lng).forecast
 
     {
-      summary: results.current_weather[:condition],
-      temperature: results.current_weather[:temperature]
+      summary: "#{results.current_weather[:condition]} #{insert_summary}",
+      temperature: "#{results.current_weather[:temperature]} F"
     }
   end
 
@@ -25,5 +27,23 @@ class BookSearch
         title: book[:title]
       }
     end
+  end
+
+  private
+
+  def insert_summary
+    summary = [
+      "with a chance of rainbows",
+      "with a hint of nothingness",
+      "with a chance of meatballs",
+      "with a chance of happiness",
+      "with a possibility of flying pigs",
+      "with a dash of dinner",
+      "with a flurry of magic",
+      "with dreams",
+      "with a touch of destiny",
+      "with a mystical aura"
+    ]
+    summary.sample
   end
 end
--- spec/requests/api/v0/book_search_spec.rb
+++ spec/requests/api/v0/book_search_spec.rb
@@ -34,7 +34,43 @@ RSpec.describe "GET /api/v0/book_search", :vcr do
       expect(response).to be_successful
       expect(response.status).to eq(200)
 
+      expect(book_search).to be_a(Hash)
       
+      expect(book_search).to have_key(:id)
+      expect(book_search[:id]).to be nil
+      
+      expect(book_search).to have_key(:type)
+      expect(book_search[:type]).to eq("books")
+
+      expect(book_search).to have_key(:attributes)
+      expect(book_search[:attributes]).to be_a(Hash)
+
+      expect(book_search[:attributes]).to have_key(:destination)
+      expect(book_search[:attributes][:destination]).to be_a(String)
+
+      expect(book_search[:attributes]).to have_key(:forecast)
+      expect(book_search[:attributes][:forecast]).to be_a(Hash)
+
+      expect(book_search[:attributes][:forecast]).to have_key(:summary)
+      expect(book_search[:attributes][:forecast][:summary]).to be_a(String)
+
+      expect(book_search[:attributes][:forecast]).to have_key(:temperature)
+      expect(book_search[:attributes][:forecast][:temperature]).to be_a(Float)
+
+      expect(book_search[:attributes]).to have_key(:total_books_found)
+      expect(book_search[:attributes][:total_books_found]).to be_a(Integer)
+      
+      expect(book_search[:attributes]).to have_key(:books)
+      expect(book_search[:attributes][:books]).to be_an(Array)
+
+      expect(book_search[:attributes][:books].count).to eq(5)
+      expect(book_search[:attributes][:books].first).to be_a(Hash)
+
+      expect(book_search[:attributes][:books].first).to have_key(:isbn)
+      expect(book_search[:attributes][:books].first[:isbn]).to be_a(Array)
+      
+      expect(book_search[:attributes][:books].first).to have_key(:title)
+      expect(book_search[:attributes][:books].first[:title]).to be_a(String)
     end
   end
 end
