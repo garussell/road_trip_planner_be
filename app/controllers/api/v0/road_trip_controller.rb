aafbfcc7d133544a79f7376762e25e5c4221cad0
Allen Russell <garyrusselljr@gmail.com>
2023-09-26 09:37:17 -0600

    refactor: reduce complexity by adding road trip valid helper method

--- app/controllers/api/v0/road_trip_controller.rb
+++ app/controllers/api/v0/road_trip_controller.rb
@@ -7,15 +7,14 @@ module Api
         user = User.authenticate(params[:api_key])
 
         return render_unauthorized unless user
-
         return render_invalid_parameters unless valid_road_trip_params?
 
         road_trip = RoadTripFacade.create_road_trip(road_trip_params)
 
-        if road_trip.travel_time.nil? || road_trip.weather_at_eta.nil?
-          render json: ErrorSerializer.format_errors('Impossible Route'), status: 404
-        else
+        if road_trip_valid?(road_trip)
           render json: RoadTripSerializer.new(road_trip), status: 200
+        else
+          render json: ErrorSerializer.format_errors('Impossible Route'), status: 404
         end
       end
 
@@ -25,6 +24,10 @@ module Api
         params.permit(:origin, :destination, :units, :api_key)
       end
 
+      def road_trip_valid?(road_trip)
+        !road_trip.travel_time.nil? && !road_trip.weather_at_eta.nil?
+      end
+      
       def valid_road_trip_params?
         !road_trip_params[:origin].empty? && !road_trip_params[:destination].empty?
       end
